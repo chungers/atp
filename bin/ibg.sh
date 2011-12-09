cf483b7c856ee2933ad2ed1d4b3f9a8f1c1e9fac
David <davidc616@gmail.com>
2011-12-08 23:57:26 -0800

    Script for starting IB Gateway on linux boxes with vncserver

---
+++ bin/ibg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function showHelp {
+    echo "$0 <options>"
+    echo " -d <dir> : runtime directory."
+    echo " -v <display> : vncserver display number"
+    exit 0;
+}
+
+INSTALL_DIR=$(dirname $0)/../third_party/interactivebrokers/ibg
+RUNTIME_DIR=$(pwd)
+DISPLAY_NUM=1
+IBG_DIR=$INSTALL_DIR/IBJts
+
+while getopts "d:v:" optionName; do
+case "$optionName" in
+d) RUNTIME_DIR="$OPTARG";;
+v) DISPLAY_NUM="$OPTARG";;
+[?]) showHelp;;
+esac
+done
+
+if [[ $(which vncserver) != "" ]]; then
+    if [[ $DISPLAY_NUM != "" ]]; then
+	echo "Start vncserver.";
+	vncserver -geometry 800x600 -depth 16 :${DISPLAY_NUM}
+	export DISPLAY=localhost:${DISPLAY_NUM}
+    fi
+fi
+
+CLASSPATH=$IBG_DIR/jts.jar:$IBG_DIR/hsqldb.jar:$IBG_DIR/jcommon-1.0.12.jar:$IBG_DIR/jhall.jar:$IBG_DIR/other.jar:$IBG_DIR/rss.jar
+java -cp $CLASSPATH  -Dsun.java2d.noddraw=true -Xmx512M ibgateway.GWClient $RUNTIME_DIR &>$RUNTIME_DIR/ibg.log
