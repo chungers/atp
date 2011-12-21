7bb795e9b6471c6fe670ceb64914eda93c8339ea
David <davidc616@gmail.com>
2011-12-20 23:11:49 -0800

    Added command args for firehose R scripts

--- R/firehose_start.R
+++ R/firehose_start.R
@@ -4,10 +4,16 @@ library(raptor)
 
 load('firehose_contracts.RData')
 
-#zmq <- raptor.zmq.connect(addr='tcp://127.0.0.1:6666', type='ZMQ_REQ')
-zmq <- raptor.zmq.connect(addr='tcp://69.164.211.61:6666', type='ZMQ_REQ')
 # request market data for all symbols
 stopifnot(is.vector(symbols))
+
+# if invoked with R --vanilla --slave --args local then use the localhost address.
+fh <- commandArgs()[5]
+ep <- ifelse(!is.na(fh) && fh == 'local', 'tcp://127.0.0.1:6666', 'tcp://69.164.211.61:6666')
+
+message('endpoint = ', ep)
+
+zmq <- raptor.zmq.connect(addr=ep, type='ZMQ_REQ')
 result <- fh_marketData(zmq=zmq, symbols=symbols)
 raptor.zmq.disconnect(zmq)
 
--- R/firehose_stop.R
+++ R/firehose_stop.R
@@ -5,7 +5,13 @@ library(raptor)
 load('firehose_contracts.RData')
 stopifnot(is.vector(symbols))
 
-zmq <- raptor.zmq.connect(addr='tcp://69.164.211.61:6666', type='ZMQ_REQ')
+# if invoked with R --vanilla --slave --args local then use the localhost address.
+fh <- commandArgs()[5]
+ep <- ifelse(!is.na(fh) && fh == 'local', 'tcp://127.0.0.1:6666', 'tcp://69.164.211.61:6666')
+
+message('endpoint = ', ep)
+
+zmq <- raptor.zmq.connect(addr=ep, type='ZMQ_REQ')
 result <- fh_cancel_marketData(zmq=zmq, symbols=symbols)
 raptor.zmq.disconnect(zmq)
 
