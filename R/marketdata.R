ff06e4be12988cdb193786d802a8780dd1b401be
gyokuro <davidc616@gmail.com>
2011-12-29 20:54:23 -0800

    Use <<- operator for changing global state.

--- R/marketdata.R
+++ R/marketdata.R
@@ -5,19 +5,15 @@ library(raptor)
 options(digits.secs=6)
 
 # convenience functions
-getVar <- function(n) { get(n, .GlobalEnv) }
-
-setVar <- function(n, v) { assign(n, v, .GlobalEnv) }
-
 nycTime <- function(t) {
   utc <- as.POSIXct(t, origin='1970-01-01', tz='GMT');
   as.POSIXlt(utc, 'America/New_York');
 }
 
-
 # if invoked with R --vanilla --slave --args local
 # then use the localhost address.
 fh <- commandArgs()[5]
+#fh <- 'local'
 ep <- ifelse(!is.na(fh) && fh == 'local',
              'tcp://127.0.0.1:7777',
              'tcp://69.164.211.61:7777')
@@ -37,30 +33,29 @@ DATA <- list()
 latencies <- c()
 #x11()
 
-tradingEnd <- ISOdatetime(2011, 12, 29, 16, 0, 0, tz='America/New_York')
+tradingEnd <- ISOdatetime(2011, 12, 30, 12, 0, 0, tz='America/New_York')
 
 marketdata.start(subscriber, function(topic, t, evt, val, delay) {
 
   lt <- nycTime(t)
   sample <- xts(as.numeric(val), lt, tzone='America/New_York')
   if (is.null(DATA[[topic]])) {
-    DATA[[topic]] <- list()
+    DATA[[topic]] <<- list()
   }
   if (is.null(DATA[[topic]][[evt]])) {
-    DATA[[topic]][[evt]] <- sample
-    names(DATA[[topic]][[evt]]) <- c(paste(topic,evt, sep=':'))
+    DATA[[topic]][[evt]] <<- sample
+    names(DATA[[topic]][[evt]]) <<- c(paste(topic,evt, sep=':'))
   } else {
-    DATA[[topic]][[evt]] <- c(DATA[[topic]][[evt]], sample)
+    DATA[[topic]][[evt]] <<- c(DATA[[topic]][[evt]], sample)
   }
-  setVar('DATA', DATA)
 
   message(paste(count, topic, lt, evt, val, delay, sep=' '))
 
-  setVar('latencies', c(latencies, delay))
-  setVar('count', count + 1)
+  latencies <<- c(latencies, delay)
+  count <<- count+1
 
   #hist(latencies)
   return(lt < tradingEnd)
 })
 
-save.image('2011_12_29_AAPL.RData')
+save.image('2011_12_30_AAPL.RData')
