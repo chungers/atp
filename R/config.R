56179ae2e945ce28baaf93bedacbd6307d339ba2
gyokuro <davidc616@gmail.com>
2012-01-09 09:35:42 -0800

    Added missing config R file

---
+++ R/config.R
@@ -0,0 +1,81 @@
+
+CONFIG <- list()
+
+CONFIG$indexes <- c(
+'INDU',
+'SPX')
+
+CONFIG$symbols <- c(
+'AAPL',
+'AKAM',
+'AMZN',
+'APC',
+'BAC',
+'BIDU',
+'BMC',
+'C',
+'CAT',
+'CMG',
+'CRM',
+'CROX',
+'DECK',
+'DDM',
+'DXD',
+'ERX',
+'ERY',
+'FAS',
+'FAZ',
+'FFIV',
+'GOOG',
+'IBM',
+'INTC',
+'ISRG',
+'JPM',
+'GLD',
+'GLL',
+'GRPN',
+'GS',
+'LNKD',
+'MS',
+'MSFT',
+'NFLX',
+'OPEN',
+'ORCL',
+'PCLN',
+'QID',
+'QLD',
+'REW',
+'RIMM',
+'RTH',
+'SMN',
+'SOHU',
+'SPY',
+'URE',
+'VMW',
+'WFM',
+'WYNN',
+'XLE',
+'XLV',
+"ZNGA")
+
+library(foreach)
+
+CONFIG$book <- c('AAPL.STK', 'GOOG.STK', 'SPY.STK')
+
+aapl_options <- foreach(strike = c(390, 400, 410, 420),
+                        .combine='c') %do% {
+  paste('AAPL.OPT', 20120113, strike, sep='.')
+}
+
+goog_options <- foreach(strike = c(640, 650, 660, 670, 680),
+                        .combine='c') %do% {
+  paste('GOOG.OPT', 20120113, strike, sep='.')
+}
+
+CONFIG$options <- c(aapl_options, goog_options)
+
+# name of the contract database
+CONFIG$contractDb <- 'ContractDb.RData'
+
+CONFIG$firehose <- list(remote = 'tcp://69.164.211.61:6666',
+                        local = 'tcp://127.0.0.1:6666')
