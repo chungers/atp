4d61a1c74be2939e7834750eda3a846236bffc3d
gyokuro <davidc616@gmail.com>
2012-01-11 01:44:10 -0800

    Missing implementation for marshalling market depth messages

--- src/raptor/src/raptor_firehose.cpp
+++ src/raptor/src/raptor_firehose.cpp
@@ -68,6 +68,56 @@ bool marshall(CancelMarketDataRequest& cmdr, List& rList)
   return true;
 }
 
+bool marshall(MarketDepthRequest& mdr, List& rList)
+{
+  mdr.set(FIX::MDEntryRefID(R_STRING(rList["conId"])));
+  mdr.set(FIX::Symbol(R_STRING(rList["symbol"])));
+  mdr.set(FIX::SecurityExchange(R_STRING(rList["exch"])));
+  mdr.set(FIX::Currency(R_STRING(rList["currency"])));
+
+  if (R_STRING(rList["sectype"]) == "STK") {
+    mdr.set(FIX::SecurityType(FIX::SecurityType_COMMON_STOCK));
+  } else if (R_STRING(rList["sectype"]) == "IND") {
+    mdr.set(FIX::SecurityType(FIX::SecurityType_INDEXED_LINKED));
+  } else if (R_STRING(rList["sectype"]) == "OPT") {
+    mdr.set(FIX::SecurityType(FIX::SecurityType_OPTION));
+    if (R_STRING(rList["right"]) == "P") {
+      mdr.set(FIX::PutOrCall(FIX::PutOrCall_PUT));
+    } else if (R_STRING(rList["right"]) == "C") {
+      mdr.set(FIX::PutOrCall(FIX::PutOrCall_CALL));
+    }
+    float strike = 0;
+    std::istringstream iss(R_STRING(rList["strike"]));
+    iss >> strike;
+    mdr.set(FIX::StrikePrice(strike));
+
+    float multiplier = 0;
+    std::istringstream iss2(R_STRING(rList["multiplier"]));
+    iss2 >> multiplier;
+    mdr.set(FIX::ContractMultiplier(multiplier));
+
+    mdr.set(FIX::DerivativeSecurityID(R_STRING(rList["local"])));
+
+    std::string expiry = R_STRING(rList["expiry"]);
+    int year = 2011;
+    int month = 12;
+    int day = 16;
+
+    if (sscanf(expiry.c_str(), "%4u%2u%2u", &year, &month, &day)) {
+      IBAPI::V964::FormatExpiry(mdr, year, month, day);
+    }
+  }
+
+  return true;
+}
+
+bool marshall(CancelMarketDepthRequest& cmdr, List& rList)
+{
+  cmdr.set(FIX::MDEntryRefID(R_STRING(rList["conId"])));
+  cmdr.set(FIX::Symbol(R_STRING(rList["symbol"])));
+  cmdr.set(FIX::DerivativeSecurityID(R_STRING(rList["local"])));
+  return true;
+}
 
 SEXP firehose_marketdata(SEXP handle, SEXP list)
 {
