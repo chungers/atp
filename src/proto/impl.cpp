576aa964d747627406d0880dcf0ba189cefa9ca0
David <davidc616@gmail.com>
2012-03-12 00:11:21 -0700

    Fix broken linux build - no template instantiations with inline

--- src/proto/impl.cpp
+++ src/proto/impl.cpp
@@ -20,10 +20,10 @@ using proto::historian::IndexedValue;
 using proto::historian::SessionLog;
 using proto::historian::Record;
 
-template inline const Record wrap<MarketData>(const MarketData& v);
-template inline const Record wrap<MarketDepth>(const MarketDepth& v);
-template inline const Record wrap<SessionLog>(const SessionLog& v);
-template inline const Record wrap<IndexedValue>(const IndexedValue& v);
+template const Record wrap<MarketData>(const MarketData& v);
+template const Record wrap<MarketDepth>(const MarketDepth& v);
+template const Record wrap<SessionLog>(const SessionLog& v);
+template const Record wrap<IndexedValue>(const IndexedValue& v);
 
 } // historian
 } // proto
