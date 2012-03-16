f9a9535898b8b075d55394b792eb04f7faad989a
David <davidc616@gmail.com>
2012-03-15 22:52:15 -0700

    Added shared libraries dependencies when deploying.

--- bin/build.sh
+++ bin/build.sh
@@ -10,8 +10,37 @@ function showHelp {
     exit 0;
 }
 
+
+function processLdd {
+    target=$1
+    deps_dir=$2
+
+    echo "Getting dependencies of $target"
+
+    platform=$(uname)
+    case "$platform" in
+    Darwin) deps=$(otool -L $target | cut -f1 -d' ' | grep ".dylib");;
+    Linux) deps=$(ldd $target | cut -f3 -d' ' | grep ".so");;
+    esac
+
+    #copy each shared library over to deps_dir
+    for i in $deps; do
+        if [ ! -f $i ]; then
+            i=$(find /usr/local/lib /usr/lib -name $i)
+        fi
+        if [ -f $i ]; then
+            echo "Copying $i to $deps_dir"
+            cp -f $i $deps_dir
+        fi
+    done
+}
+
 # main
 DIR=$(dirname $0)/..
+cd $DIR
+DIR=$(pwd)
+
+echo "Running from $DIR"
 
 CLEAN=0
 BUILD=0
@@ -20,6 +49,7 @@ DEPLOY_HUB="$DIR/../hub"
 DEPLOY_HUB_ATP="$DEPLOY_HUB/atp"
 DEPLOY=0
 MESSAGE=""
+DO_COMMIT=0
 
 while getopts "t:m:cad" optionName; do
 case "$optionName" in
@@ -27,7 +57,7 @@ c) CLEAN=1;;
 t) TARGET="$OPTARG"; BUILD=1;;
 a) BUILD=1;;
 d) DEPLOY=1;;
-m) MESSAGE="$OPTARG";;
+m) MESSAGE="$OPTARG";DO_COMMIT=1;;
 [?]) showHelp;;
 esac
 done
@@ -76,14 +106,24 @@ if [ $DEPLOY == 1 ]; then
     rm -rf $DIR/install/
     make install
     mkdir -p $DEPLOY_TARGET
+    mkdir -p $DEPLOY_TARGET/deps
     cp -r $DIR/install/* $DEPLOY_TARGET
-    pushd $DEPLOY_HUB/
-    git add -v atp/
-    git status
-    echo "Committing with message: $MESSAGE"
-    git commit -m "Build / release: $MESSAGE" -a
-    git push
-    popd
+
+    # determine the shared library dependencies
+    l=$(ls $DEPLOY_TARGET/bin)
+    for i in $l; do
+        processLdd $DEPLOY_TARGET/bin/$i $DEPLOY_TARGET/deps
+    done
+
+    if [ $DO_COMMIT == 1 ]; then
+        pushd $DEPLOY_HUB/
+        git add -v atp/
+        git status
+        echo "Committing with message: $MESSAGE"
+        git commit -m "Build / release: $MESSAGE" -a
+        git push
+        popd
+    fi
 fi
 
 popd
