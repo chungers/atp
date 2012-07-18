c6266532227d88a05ef158a43a85437253ddd4a1
gyokuro <davidc616@gmail.com>
2012-07-17 22:40:51 -0700

    Refactor em main

---
+++ src/main/em.hpp
@@ -0,0 +1,66 @@
+#ifndef MAIN_EM_H_
+#define MAIN_EM_H_
+
+#include <map>
+#include <set>
+#include <string>
+
+#include <boost/assign.hpp>
+
+#include "ib/ApplicationBase.hpp"
+
+using std::map;
+using std::set;
+using std::string;
+using IBAPI::ApiEventDispatcher;
+using IBAPI::SessionID;
+
+
+/// format:  {session_id}={gateway_ip_port}@{reactor_endpoint}
+const string CONNECTOR_SPECS =
+    "200=127.0.0.1:4001@tcp://127.0.0.1:6667";
+
+const string OUTBOUND_ENDPOINTS =
+    "0=tcp://127.0.0.1:8888";
+
+// ExecutionManager only supports messages related to orders
+const set<string> EM_VALID_MESSAGES_ =
+               boost::assign::list_of
+               ("IBAPI.EXEC.CancelOrder")
+               ("IBAPI.EXEC.MarketOrder")
+               ("IBAPI.EXEC.LimitOrder")
+               ("IBAPI.EXEC.StopLimitOrder")
+               ;
+
+class ExecutionManager : public IBAPI::ApplicationBase
+{
+ public:
+
+  ExecutionManager() {}
+  ~ExecutionManager() {}
+
+
+  virtual bool IsMessageSupported(const string& key)
+  {
+    return EM_VALID_MESSAGES_.find(key) != EM_VALID_MESSAGES_.end();
+  }
+
+  virtual ApiEventDispatcher* GetApiEventDispatcher(const SessionID& sessionId)
+  {
+    return new ib::internal::OrderEventDispatcher(*this, sessionId);
+  }
+
+  void onLogon(const SessionID& sessionId)
+  {
+    LOG(INFO) << "Session " << sessionId << " logged on.";
+  }
+
+  void onLogout(const SessionID& sessionId)
+  {
+    LOG(INFO) << "Session " << sessionId << " logged off.";
+  }
+
+};
+
+
+#endif //MAIN_EM_H_
--- src/main/em_main.cpp
+++ src/main/em_main.cpp
@@ -22,16 +22,20 @@
 #include "ib/SocketInitiator.hpp"
 #include "ib/OrderEventDispatcher.hpp"
 
+#include "em.hpp"
+
+using std::map;
+using std::vector;
+using std::string;
+using std::stringstream;
+using std::istringstream;
+using IBAPI::ApiEventDispatcher;
+using IBAPI::SessionID;
+using IBAPI::SessionSetting;
+using IBAPI::SocketInitiator;
 
 static IBAPI::SocketInitiator* INITIATOR_INSTANCE;
 static atp::varz::VarzServer* VARZ_INSTANCE;
-
-
-/// format:  {session_id}={gateway_ip_port}@{reactor_endpoint}
-const std::string CONNECTOR_SPECS =
-    "200=127.0.0.1:4001@tcp://127.0.0.1:6667";
-
-const std::string OUTBOUND_ENDPOINTS = "0=tcp://127.0.0.1:8888";
 
 DEFINE_string(connectors, CONNECTOR_SPECS,
               "Comma-delimited list of gateway ip/port @ control endpoints.");
@@ -62,56 +66,6 @@ void OnTerminate(int param)
   LOG(INFO) << "Bye.";
   exit(1);
 }
-
-// Firehose only supports messages related to market data.
-const set<string> EM_VALID_MESSAGES_ =
-               boost::assign::list_of
-               ("IBAPI.EXEC.CancelOrder")
-               ("IBAPI.EXEC.MarketOrder")
-               ("IBAPI.EXEC.LimitOrder")
-               ("IBAPI.EXEC.StopLimitOrder")
-               ;
-
-using std::map;
-using std::vector;
-using std::string;
-using std::stringstream;
-using std::istringstream;
-using IBAPI::ApiEventDispatcher;
-using IBAPI::SessionID;
-using IBAPI::SessionSetting;
-using IBAPI::SocketInitiator;
-
-
-class ExecutionManager : public IBAPI::ApplicationBase
-{
- public:
-
-  ExecutionManager() {}
-  ~ExecutionManager() {}
-
-
-  virtual bool IsMessageSupported(const std::string& key)
-  {
-    return EM_VALID_MESSAGES_.find(key) != EM_VALID_MESSAGES_.end();
-  }
-
-  virtual ApiEventDispatcher* GetApiEventDispatcher(const SessionID& sessionId)
-  {
-    return new ib::internal::OrderEventDispatcher(*this, sessionId);
-  }
-
-  void onLogon(const IBAPI::SessionID& sessionId)
-  {
-    LOG(INFO) << "Session " << sessionId << " logged on.";
-  }
-
-  void onLogout(const IBAPI::SessionID& sessionId)
-  {
-    LOG(INFO) << "Session " << sessionId << " logged off.";
-  }
-
-};
 
 
 ////////////////////////////////////////////////////////
