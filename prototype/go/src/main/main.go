a6e3f32a9916d7d7e13c94804549867bb05e8cc9
gyokuro <davidc616@gmail.com>
2013-04-27 00:59:57 -0700

    Code cleanup to remove global variables

--- prototype/go/src/main/main.go
+++ prototype/go/src/main/main.go
@@ -1,39 +1,31 @@
 package main
 
 import (
-	"github.com/hoisie/web"
 	"borg"
+	"github.com/hoisie/web"
 	"log"
 	"os"
 )
 
-var glogger *log.Logger
-var server *web.Server
-
-func exitServer() string {
-	glogger.Println("Shutting down")
-	defer server.Close()
-	return "ok"
-}
-
 func shutdown(server *web.Server) func() string {
 	return func() string {
-		glogger.Println("Shutdown ", server)
+		server.Logger.Println("Shutdown server at port", server.Config)
 		defer server.Close()
 		return "stopped"
 	}
 }
 
-func hello(val string) string {
+func hello(ctx *web.Context, val string) string {
 	borg.Borg()
+	ctx.Server.Logger.Println("hello ==> " + val)
 	return "Hello " + val
 }
 
 func hello2(ctx *web.Context, val string) string {
 	path := ctx.Request.URL.Path
-	glogger.Println("Got path = ", path)
+	ctx.Server.Logger.Println("Got path = ", path)
 	for k, v := range ctx.Params {
-		glogger.Println(k, v)
+		ctx.Server.Logger.Println(k, v)
 	}
 	return path + " " + val
 }
@@ -45,13 +37,13 @@ func main() {
 		return
 	}
 
-	glogger = log.New(f, "", log.Ldate|log.Ltime)
+	server := web.NewServer()
 
-	server = web.NewServer()
-	server.SetLogger(glogger)
+	logger := log.New(f, "", log.Ldate|log.Ltime)
+	server.SetLogger(logger)
 
-	server.Get("/exit", exitServer)
 	server.Get("/shutdown", shutdown(server))
+	server.Get("/hello/(.*)", hello)
 	server.Get("/hello2/(.*)", hello2)
 	server.Put("/hello2/(.*)", hello2)
 	server.Get("/(.*)", hello)
