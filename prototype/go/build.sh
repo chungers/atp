b2e7229339af30161f197f559f7a39f17827a3d1
gyokuro <davidc616@gmail.com>
2013-04-28 11:05:43 -0700

    Adding protobuff

--- prototype/go/build.sh
+++ prototype/go/build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-export GOPATH=`pwd`:$GOPATH
 
+ATP_DIR=$(pwd)/../..
+PROTOTYPE_DIR=$ATP_DIR/prototype
+PROTO_DIR=$PROTOTYPE_DIR/proto
+
+
+export GOPATH=`pwd`:$GOPATH
 echo "GOPATH=$GOPATH"
 
-# set up required packages
-go get github.com/hoisie/web
+export PATH=$(pwd)/bin:$PATH
+echo "PATH=$PATH"
 
+# set up required packages
+go get -u github.com/hoisie/web
+go get -u code.google.com/p/goprotobuf/...
+
+# build the protocol buffers
+protoc --go_out=src --proto_path=$PROTO_DIR $PROTO_DIR/*.proto
+
+# build the main
 go build src/main/main.go
 
